ef0d12ca2e6ca92edae4bd12134d6933c0590757
Goran Kecman <lestrigonac@hotmail.com>
2018-04-04 12:36:17 +0200

    Created LED service to communicate over BLE. New required libraries added - nRF51822 and BLE_API

--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,110 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2006-2013 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 #include "mbed.h"
+#include "ble/BLE.h"
+#include "LEDService.h"
 #include "app_config.h"
-#include "ISL1208.h"
-
-DigitalOut led1(LED_1);
-DigitalOut led2(LED_2);
-DigitalOut led3(LED_3);
-InterruptIn button1(BUTTON_1);
-
-//Serial pc(USBTX, USBRX);
-Serial pc(UART_TX, UART_RX);
-// Create I2C object
-I2C i2c(I2C_SDA, I2C_SCL);
-// Create ISL1208 object
-ISL1208 rtc(&i2c);    
-
-void button1_detect(void)
+ 
+DigitalOut alivenessLED(LED_1, 0);
+DigitalOut actuatedLED(LED_3, 1);
+ 
+const static char     DEVICE_NAME[] = "LED";
+static const uint16_t uuid16_list[] = {LEDService::LED_SERVICE_UUID};
+ 
+LEDService *ledServicePtr;
+ 
+Ticker ticker;
+ 
+void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params)
 {
-    pc.printf("Button pressed...\n");
-    led2 = !led2;
+    BLE::Instance().gap().startAdvertising();
 }
-
-int main() 
+ 
+void periodicCallback(void)
 {
-    //Initialize variables
-    pc.baud(115200);
-    pc.printf("Start...\n");
-
-    i2c.frequency(400000); //400kHz
-
-    button1.fall(button1_detect);
-    led1 = LED_OFF;
-    led2 = LED_OFF;
-    led3 = LED_OFF;
-
-    //Try to open the ISL1208
-    if (rtc.open()) 
-    {
-        led1 = LED_ON;
-        pc.printf("Device detected!\n");
+    alivenessLED = !alivenessLED; /* Do blinky on LED1 to indicate system aliveness. */
+}
  
-        //Configure the oscillator for a 32.768kHz crystal
-        rtc.oscillatorMode(ISL1208::OSCILLATOR_CRYSTAL);
+/**
+ * This callback allows the LEDService to receive updates to the ledState Characteristic.
+ *
+ * @param[in] params
+ *     Information about the characterisitc being updated.
+ */
+void onDataWrittenCallback(const GattWriteCallbackParams *params) {
+    if ((params->handle == ledServicePtr->getValueHandle()) && (params->len == 1)) {
+        actuatedLED = *(params->data);
+    }
+}
  
-        // Reset the time 
-        //rtc.time(1522407240);
-
-        //Check if we need to reset the time
-        if (rtc.powerFailed()) 
-        {
-            led3 = LED_ON;
-            //The time has been lost due to a power complete power failure
-            pc.printf("Device has lost power! Resetting time...\n");
-
-            //Set RTC time to Wed, 28 Oct 2009 11:35:37
-            rtc.time(1522758120);
-        }
-
-        while(1) 
-        {
-            led2 = !led2;
-            //Get the current time
-            time_t seconds = rtc.time();
+/**
+ * This function is called when the ble initialization process has failed
+ */
+void onBleInitError(BLE &ble, ble_error_t error)
+{
+    /* Initialization error handling should go here */
+}
  
-            //Print the time in various formats
-            pc.printf("\nTime as seconds since January 1, 1970 = %d\n", seconds);
-            pc.printf("Time as a basic string = %s", ctime(&seconds));
-            char buffer[32];
-            strftime(buffer, 32, "%I:%M %p\n", localtime(&seconds));
-            pc.printf("Time as a custom formatted string = %s", buffer);
+/**
+ * Callback triggered when the ble initialization process has finished
+ */
+void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
+{
+    BLE&        ble   = params->ble;
+    ble_error_t error = params->error;
  
-            //Delay for 1.0 seconds
-            wait(1.0);
-        }
-    } 
-    else 
-    {
-        pc.printf("Device not detected!\n");
-        led3 = LED_ON;
-    } 
-
-    return 0;
+    if (error != BLE_ERROR_NONE) {
+        /* In case of error, forward the error handling to onBleInitError */
+        onBleInitError(ble, error);
+        return;
+    }
+ 
+    /* Ensure that it is the default instance of BLE */
+    if(ble.getInstanceID() != BLE::DEFAULT_INSTANCE) {
+        return;
+    }
+ 
+    ble.gap().onDisconnection(disconnectionCallback);
+    ble.gattServer().onDataWritten(onDataWrittenCallback);
+ 
+    bool initialValueForLEDCharacteristic = false;
+    ledServicePtr = new LEDService(ble, initialValueForLEDCharacteristic);
+ 
+    /* setup advertising */
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(uuid16_list));
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
+    ble.gap().setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
+    ble.gap().setAdvertisingInterval(1000); /* 1000ms. */
+    ble.gap().startAdvertising();
+}
+ 
+int main(void)
+{
+    ticker.attach(periodicCallback, 1); /* Blink LED every second */
+ 
+    BLE &ble = BLE::Instance();
+    ble.init(bleInitComplete);
+ 
+    /* SpinWait for initialization to complete. This is necessary because the
+     * BLE object is used in the main loop below. */
+    while (ble.hasInitialized()  == false) { /* spin loop */ }
+ 
+    while (true) {
+        ble.waitForEvent();
+    }
 }
