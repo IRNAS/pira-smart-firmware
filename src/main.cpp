22a24b99bb18aee0f8c06a0dc5b4376f7adc1eac
Goran Kecman <lestrigonac@hotmail.com>
2018-04-05 14:41:10 +0200

    Basic usage of LED service and RTC

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,17 @@
 #include "ble/BLE.h"
 #include "LEDService.h"
 #include "app_config.h"
+#include "ISL1208.h"
  
 DigitalOut alivenessLED(LED_1, 0);
 DigitalOut actuatedLED(LED_3, 1);
+
+// Create UART object
+Serial pc(UART_TX, UART_RX);
+// Create I2C object
+I2C i2c(I2C_SDA, I2C_SCL);
+// Create ISL1208 object
+ISL1208 rtc(&i2c);    
  
 const static char     DEVICE_NAME[] = "LED";
 static const uint16_t uuid16_list[] = {LEDService::LED_SERVICE_UUID};
@@ -28,7 +36,9 @@ static const uint16_t uuid16_list[] = {LEDService::LED_SERVICE_UUID};
 LEDService *ledServicePtr;
  
 Ticker ticker;
- 
+
+uint8_t send_time; 
+
 void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params)
 {
     BLE::Instance().gap().startAdvertising();
@@ -37,6 +47,16 @@ void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params)
 void periodicCallback(void)
 {
     alivenessLED = !alivenessLED; /* Do blinky on LED1 to indicate system aliveness. */
+    
+    send_time = 1; 
+    //Get the current time
+    //time_t seconds = rtc.time();
+    //pc.printf("Test\n");
+    //Print the time in various formats
+    //pc.printf("Time as a basic string = %s", ctime(&seconds));
+    //char buffer[32];
+    //strftime(buffer, 32, "%I:%M %p\n", localtime(&seconds));
+    //pc.printf("Time as a custom formatted string = %s", buffer);
 }
  
 /**
@@ -92,11 +112,53 @@ void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
     ble.gap().setAdvertisingInterval(1000); /* 1000ms. */
     ble.gap().startAdvertising();
 }
+
+void init_uart(void)
+{
+    pc.baud(115200);
+    pc.printf("Start...\n");
+}
+
+void init_rtc(void)
+{
+    i2c.frequency(400000); //400kHz
+    
+    //Try to open the ISL1208
+    if (rtc.open()) 
+    {
+        pc.printf("Device detected!\n");
+ 
+        //Configure the oscillator for a 32.768kHz crystal
+        rtc.oscillatorMode(ISL1208::OSCILLATOR_CRYSTAL);
+ 
+        //Check if we need to reset the time
+        if (rtc.powerFailed()) 
+        {
+            //The time has been lost due to a power complete power failure
+            pc.printf("Device has lost power! Resetting time...\n");
+
+            //Set RTC time to Wed, 28 Oct 2009 11:35:37
+            rtc.time(1522758120);
+        }
+    }
+    else
+    {
+        pc.printf("Device NOT detected!\n");
+    }
+}
  
 int main(void)
 {
-    ticker.attach(periodicCallback, 1); /* Blink LED every second */
+    send_time = 0;
+    // UART needs to be initialized first to use it for debugging
+    init_uart();
+
+    // I2C and RTC init
+    init_rtc();
  
+    // periodicCallback must be attached after I2C is initialized
+    ticker.attach(periodicCallback, 1); /* Blink LED every second */
+
     BLE &ble = BLE::Instance();
     ble.init(bleInitComplete);
  
@@ -106,5 +168,18 @@ int main(void)
  
     while (true) {
         ble.waitForEvent();
+
+        if (send_time)
+        {
+            send_time = 0;
+
+            //Get the current time
+            time_t seconds = rtc.time();
+            //Print the time in various formats
+            pc.printf("Time as a basic string = %s", ctime(&seconds));
+            //char buffer[32];
+            //strftime(buffer, 32, "%I:%M %p\n", localtime(&seconds));
+            //pc.printf("Time as a custom formatted string = %s", buffer);
+        }
     }
 }
